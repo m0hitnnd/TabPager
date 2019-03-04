2825a4da600c2aa072ea90c87d373d69ed635a7f
mohit anand <mohit.anand@paytmmoney.com>
2019-03-04 18:20:52 +0530

    initial xcode proj added

---
+++ TabPager/TabPager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabPager
+//
+//  Created by Mohit Anand on 04/03/19.
+//  Copyright © 2019 Bugsy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
