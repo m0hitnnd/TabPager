2bdebdfaf11ec7f4f528d2add2cbf2ae16e42c91
mohitnnf <mohitanand95.ma@gmail.com>
2019-03-06 17:30:31 +0530

    added tab bar view cell

---
+++ TabPager/TabPager/TabPagerCore/TabsView.swift
@@ -0,0 +1,12 @@
+//
+//  TabsView.swift
+//  TabPager
+//
+//  Created by Mohit Anand on 05/03/19.
+//  Copyright © 2019 Bugsy. All rights reserved.
+//
+
+import UIKit
+
+class TabsView: UIView {
+}
